9be13e20e5252a5f9ba7e4c93c0581ee0b9007ba
Sam Souder <samsouder@gmail.com>
2010-07-08 12:47:42 -0500

    Changed path completion tests to be up to date and work locally for me.

--- Support/test/tc_path_complete.rb
+++ Support/test/tc_path_complete.rb
@@ -8,19 +8,19 @@ class TestPathComplete < Test::Unit::TestCase
   # end
   
   def test_initial_dir
-    result = PathComplete::Path.new({ :input => 'include_once("/we");', :project_dir => '/Users/dbergey/repos/bitleap/branches/unstable', :file_path => '/Users/dbergey/repos/bitleap/branches/unstable/web/customer/include/config.php'}).run
+    result = PathComplete::Path.new({ :input => 'include_once("/we");', :project_dir => '/Users/ssouder/repos/bitleap', :file_path => '/Users/ssouder/repos/bitleap/web/customer/include/config.php'}).run
     expected = 'include_once("/web");'
     assert_equal(expected, result)
   end
 
   def test_second_dir
-    result = PathComplete::Path.new({ :input => 'include_once("/web/cus");', :project_dir => '/Users/dbergey/repos/bitleap/branches/unstable', :file_path => '/Users/dbergey/repos/bitleap/branches/unstable/web/customer/include/config.php'}).run
+    result = PathComplete::Path.new({ :input => 'include_once("/web/cus");', :project_dir => '/Users/ssouder/repos/bitleap', :file_path => '/Users/ssouder/repos/bitleap/web/customer/include/config.php'}).run
     expected = 'include_once("/web/customer");'
     assert_equal(expected, result)
   end
   
   def test_second_dir_with_base_path
-    result = PathComplete::Path.new({ :input => 'include_once(BASE_PATH ."/web/cus");', :project_dir => '/Users/dbergey/repos/bitleap/branches/unstable', :file_path => '/Users/dbergey/repos/bitleap/branches/unstable/web/customer/include/config.php'}).run
+    result = PathComplete::Path.new({ :input => 'include_once(BASE_PATH ."/web/cus");', :project_dir => '/Users/ssouder/repos/bitleap', :file_path => '/Users/ssouder/repos/bitleap/web/customer/include/config.php'}).run
     expected = 'include_once(BASE_PATH ."/web/customer");'
     assert_equal(expected, result)
   end
